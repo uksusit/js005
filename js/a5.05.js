942d22c52656c03570c01a1cb8d51c3cae03ee02
uksusit <140089654+uksusit@users.noreply.github.com>
2024-12-06 15:37:09 +0200

    5.05
    5

---
+++ js/a5.05.js
@@ -0,0 +1,47 @@
+const ancestor = {
+    name: "Paul",
+    age: 83,
+    surname: "Dawson",
+    heritage: "Irish",
+  };
+  // Change code below this line
+  
+  const parent = Object.create(ancestor);
+  // {
+  // name: "Stacey",
+  // surname: "Moore",
+  // age: 54,
+  // };
+  
+  const child = Object.create(parent);
+  // {};
+  child.name = "Jason";
+  child.age = 27;
+  
+  // Change code above this line
+
+  
+
+//   Зміни код, побудувавши ланцюжок прототипів таким чином, щоб об'єкт ancestor був прототипом для parent, а той, своєю чергою, був прототипом для child.
+
+// Оголошена змінна ancestor
+// Значення змінної ancestor - це об'єкт.
+// Оголошена змінна parent
+// Значення змінної parent - це об'єкт.
+// Оголошена змінна child
+// Значення змінної child - це об'єкт.
+// Виклик ancestor.isPrototypeOf("parent") повертає true
+// Виклик parent.isPrototypeOf("child") повертає true
+// Виклик ancestor.hasOwnProperty("surname") повертає true
+// Звернення до ancestor.surname повертає "Dawson"
+// Виклик parent.hasOwnProperty("surname") повертає true
+// Звернення до parent.surname повертає "Moore"
+// Виклик child.hasOwnProperty("surname") повертає false
+// Звернення до child.surname повертає "Moore"
+// Виклик ancestor.hasOwnProperty("heritage") повертає true
+// Звернення до ancestor.heritage повертає "Irish"
+// Виклик parent.hasOwnProperty("heritage") повертає false
+// Звернення до parent.heritage повертає "Irish"
+// Виклик child.hasOwnProperty("heritage") повертає false
+// Звернення до child.heritage повертає "Irish"
+// Використовується метод Object.create()
