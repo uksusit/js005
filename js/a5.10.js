f7ec5ab5907f1158d35773d73376aaa829613ebe
uksusIT <140089654+uksusit@users.noreply.github.com>
2024-12-29 17:28:39 +0200

    tmp5.10

--- js/a5.10.js
+++ js/a5.10.js
@@ -1,25 +1,47 @@
 
-class storage {
-    constructor(params) {
-         // Метод getшitems
-    getItems() {
+class Storage { 
+    constructor(items) {
+       this.items = items;
+      // this.item = item;
+  
+    } 
+        // Метод getшitems
+      getItems()
+    {
       return this.items;
     }
-       // Метод addItem
-   addItem(newItem) {
+
+           // Метод addItem
+    addItem(newItem)
+   {
       this.items.push(newItem);
     }
-  
-      removeItem(itemToRemove) {
-        
+
+    // metood removeItem
+    removeItem(itemToRemove)
+      { 
+         console.log(itemToRemove);
+         console.log(this.items);
+         
+         sessionStorage.removeItem(itemToRemove);
       }
-    }
   }
   
   // Change code above this line
   const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+  console.log(storage);
   console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
   storage.addItem("Droid");
   console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
   storage.removeItem("Prolonger");
   console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+
+// приклад видалення з масиву
+
+//   public static int[] removeItem(int[] numbers, int index){
+//     for (int i = index; i < numbers.length -1; i++) {
+//         numbers[i] = numbers[i + 1];
+//     }
+//     return Arrays.copyOf(numbers, numbers.length - 1);
+// }
