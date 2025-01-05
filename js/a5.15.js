8a1587d243c0f714e79dbc9d1ce9c1908ec755a0
uksusIT <140089654+uksusit@users.noreply.github.com>
2025-01-05 15:04:55 +0200

    15

---
+++ js/a5.15.js
@@ -0,0 +1,99 @@
+
+
+
+// Завдання 15/20
+// Гетери та сетери — це коротший синтаксис оголошення методів для взаємодії з властивостями. Геттер і сетер імітують звичайну публічну властивість класу, але дозволяють змінювати інші властивості зручнішим способом. Геттер виконується при спробі отримати значення властивості, а сетер - при спробі його змінити.
+
+// Гетери та сетери доречно використовувати для простих операцій читання і зміни значення властивостей, особливо приватних, як їх публічний інтерфейс. Для роботи з властивістю, яка зберігає масив або об'єкт, вони не підійдуть.
+
+// class User {
+//   #email;
+
+//   constructor({ name, email }) {
+//     this.name = name;
+//     this.#email = email;
+//   }
+
+//   // Геттер email
+//   get email() {
+//     return this.#email;
+//   }
+
+//   // Сеттер email
+//   set email(newEmail) {
+//     this.#email = newEmail;
+//   }
+// }
+// Ми оголосили гетер і сетер email, поставивши перед ім'ям властивості ключові слова get і set. Всередині цих методів ми або повертаємо значення приватної властивості #email, або змінюємо її значення. Геттер і сетер застосовуються в парі та повинні називатися однаково.
+
+// const mango = new User({ name: "Mango", email: "mango@mail.com" });
+// console.log(mango.email); // mango@mail.com
+// mango.email = "mango@supermail.com";
+// console.log(mango.email); // mango@supermail.com
+// Звертаючись до mango.email, викликається гетер get email() {...} і виконується його код. При спробі запису mango.email = "mango@supermail.com" викликається сетер set email(newEmail) {...} і рядок "mango@supermail.com" буде значенням параметра newEmail.
+
+// Перевага в тому, що це методи, а значить, під час запису можна виконати додатковий код, наприклад, з будь-якими перевірками, на відміну від виконання цієї ж операції безпосередньо з властивістю.
+
+// set email(newEmail) {
+//   if(newEmail === "") {
+//     console.error("Помилка! Пошта не може бути порожнім рядком!");
+//     return;
+//   }
+
+//   this.#email = newEmail;
+// }
+
+
+// Завдання 15/20
+// Виконай рефакторинг класу Car. Зроби властивості model і price приватними, а також #brand. Стандартизуй публічний інтерфейс класу, замінивши вже оголошені методи на гетери та сетери brand, model і price, для взаємодії з приватними властивостями.
+
+// Оголошений клас Car
+// В класі Car оголошена приватна властивість brand
+// В класі Car оголошена приватна властивість model
+// В класі Car оголошена приватна властивість price
+// Конструктор класу приймає об'єкт з властивостями brand, model і price
+// В класі Car оголошений гетер brand
+// В класі Car оголошений сетер brand
+// В класі Car оголошений гетер model
+// В класі Car оголошений сетер model
+// В класі Car оголошений гетер price
+// В класі Car оголошений сетер price
+
+// До завдання
+
+class Car {
+    // Change code below this line
+    #brand;
+  
+    constructor({ brand, model, price }) {
+      this.#brand = brand;
+      this.model = model;
+      this.price = price;
+    }
+  
+    getBrand() {
+      return this.#brand;
+    }
+  
+    changeBrand(newBrand) {
+      this.#brand = newBrand;
+    }
+  
+    getModel() {
+      return this.model;
+    }
+  
+    updateModel(newModel) {
+      this.model = newModel;
+    }
+  
+    getPrice() {
+      return this.price;
+    }
+  
+    setPrice(newPrice) {
+      this.price = newPrice;
+    }
+    // Change code above this line
+  }
+  
