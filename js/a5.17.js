c4e195f3d8568b5534fc4451ab98c880bcf6d4e2
uksusit <140089654+uksusit@users.noreply.github.com>
2025-01-11 10:57:41 +0200

    17ok

---
+++ js/a5.17.js
@@ -0,0 +1,60 @@
+// Завдання 17/20
+// В класі можна оголосити не тільки методи майбутнього екземпляра, а також методи, доступні тільки класу - статичні методи, які можуть бути як публічні, так і приватні. Синтаксис оголошення аналогічний статичним властивостям, за винятком того, що значенням буде метод.
+
+// class User {
+//   static #takenEmails = [];
+
+//   static isEmailTaken(email) {
+//     return User.#takenEmails.includes(email);
+//   }
+
+//   #email;
+
+//   constructor({ email }) {
+//     this.#email = email;
+//     User.#takenEmails.push(email);
+//   }
+// }
+
+// const mango = new User({ email: "mango@mail.com" });
+
+// console.log(User.isEmailTaken("poly@mail.com"));
+// console.log(User.isEmailTaken("mango@mail.com"));
+// Особливість статичних методів полягає у тому, що під час їх виклику ключове слово this посилається на сам клас. Це означає, що статичний метод може отримати доступ до статичних властивостей класу, але не до властивостей екземпляра. Логічно, тому що статичні методи викликає сам клас, а не його екземпляри.
+
+
+// До завдання
+
+// Завдання 17/20
+// Додай класу Car публічний статичний метод checkPrice (price), що приймає ціну автомобіля. Метод повинен порівняти значення параметра price і приватної статичного властивості MAX_PRICE.
+
+// Якщо ціна автомобіля перевищує максимальну, метод повинен повернути рядок "Error! Price exceeds the maximum".
+// В іншому випадку метод повинен повернути рядок "Success! Price is within acceptable limits".
+// Під оголошенням класу ми додали ініціалізацію екземпляра і виклики методів, щоб показати, як буде використовуватися метод checkPrice(price).
+
+// Оголошений клас Car
+// Клас Car містить статичний метод checkPrice(price)
+// Виклик Car.checkPrice(36000) повертає рядок "Success! Price is within acceptable limits"
+// Виклик Car.checkPrice(18000) повертає рядок "Success! Price is within acceptable limits"
+// Виклик Car.checkPrice(64000) повертає рядок "Error! Price exceeds the maximum"
+// Виклик Car.checkPrice(57000) повертає рядок "Error! Price exceeds the maximum"
+
+class Car {
+    static #MAX_PRICE = 50000;
+    // Change code below this line
+    static checkPrice(price) {
+        if (price > Car.#MAX_PRICE) return "Error! Price exceeds the maximum";
+        else return "Success! Price is within acceptable limits";
+      };
+    // Change code above this line
+    constructor({ price }) {
+      this.price = price;
+    }
+  }
+  
+  const audi = new Car({ price: 36000 });
+  const bmw = new Car({ price: 64000 });
+  
+  console.log(Car.checkPrice(audi.price)); // "Success! Price is within acceptable limits"
+  console.log(Car.checkPrice(bmw.price)); // "Error! Price exceeds the maximum"
+  
