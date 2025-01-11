66b75c5ffa66a18ffda5e438438b5755adf29d25
uksusit <140089654+uksusit@users.noreply.github.com>
2025-01-11 09:36:46 +0200

    15ok

--- js/a5.15.js
+++ js/a5.15.js
@@ -62,38 +62,40 @@
 // До завдання
 
 class Car {
-    // Change code below this line
-    #brand;
-  
-    constructor({ brand, model, price }) {
-      this.#brand = brand;
-      this.model = model;
-      this.price = price;
-    }
-  
-    getBrand() {
-      return this.#brand;
-    }
-  
-    changeBrand(newBrand) {
-      this.#brand = newBrand;
-    }
-  
-    getModel() {
-      return this.model;
-    }
-  
-    updateModel(newModel) {
-      this.model = newModel;
-    }
-  
-    getPrice() {
-      return this.price;
-    }
-  
-    setPrice(newPrice) {
-      this.price = newPrice;
-    }
-    // Change code above this line
+  // Change code below this line
+  #brand;
+  #model;
+  #price;
+
+  constructor({ brand, model, price }) {
+    this.#brand = brand;
+    this.#model = model;
+    this.#price = price;
   }
+
+  get brand() {
+    return this.#brand;
+  }
+
+  set brand(newBrand) {
+    this.#brand = newBrand;
+  }
+
+  get model() {
+    return this.#model;
+  }
+
+  set model(newModel) {
+    this.#model = newModel;
+  }
+
+  get price() {
+    return this.#price;
+  }
+
+  set price(newPrice) {
+    this.#price = newPrice;
+  }
+  // Change code above this line
+}
   
