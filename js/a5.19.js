8d01477af9f43cf336c7b731675650407a150f2f
uksusit <140089654+uksusit@users.noreply.github.com>
2025-01-15 18:27:15 +0200

    19ok

---
+++ js/a5.19.js
@@ -0,0 +1,38 @@
+class User {
+    email;
+  
+    constructor(email) {
+      this.email = email;
+    }
+  
+    get email() {
+      return this.email;
+    }
+  
+    set email(newEmail) {
+      this.email = newEmail;
+    }
+  }
+  
+  class Admin extends User {
+    // Change code below this line
+  constructor({email, accessLevel}) {
+      super(email);
+  
+  }
+    static AccessLevel = {
+      BASIC: "basic",
+      SUPERUSER: "superuser",
+    };
+  
+    // Change code above this line
+  }
+  
+  const mango = new Admin({
+    email: "mango@mail.com",
+    accessLevel: Admin.AccessLevel.SUPERUSER,
+  });
+  
+  console.log(mango.email); // "mango@mail.com"
+  console.log(mango.accessLevel); // "superuser"
+  
