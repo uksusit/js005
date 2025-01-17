459e315c6b51c96584d46c766e667798168d1948
uksusit <140089654+uksusit@users.noreply.github.com>
2025-01-17 17:34:05 +0200

    20tmp

--- js/a5.19.js
+++ js/a5.19.js
@@ -1,3 +1,37 @@
+// Завдання 19/20
+// Насамперед в конструкторі дочірнього класу необхідно викликати спеціальну функцію super(аргументи) - це псевдонім конструктора батьківського класу. В іншому випадку, при спробі звернутися до this в конструкторі дочірнього класу, виникне помилка. Під час виклику конструктора батьківського класу передаємо необхідні йому аргументи для ініціалізації властивостей.
+
+// class User {
+//   constructor(email) {
+//     this.email = email;
+//   }
+
+//   get email() {
+//     return this.email;
+//   }
+
+//   set email(newEmail) {
+//     this.email = newEmail;
+//   }
+// }
+
+// class ContentEditor extends User {
+//   constructor({ email, posts }) {
+//     // Виклик конструктора батьківського класу User
+//     super(email);
+//     this.posts = posts;
+//   }
+// }
+
+// const editor = new ContentEditor({ email: "mango@mail.com", posts: [] });
+// console.log(editor); // { email: 'mango@mail.com', posts: [] }
+// console.log(editor.email); // 'mango@mail.com'
+
+// До завдання
+
+
+
+
 class User {
     email;
   
@@ -35,4 +69,18 @@ class User {
   
   console.log(mango.email); // "mango@mail.com"
   console.log(mango.accessLevel); // "superuser"
+
   
+//   Завдання 19/20
+// Додай класу Admin метод constructor, який приймає один параметр - об'єкт налаштувань з двома властивостями email і accessLevel. Додай класу Admin публічну властивість accessLevel, значення якої буде передаватися під час виклику конструктора.
+
+// Щоб показати, як буде використовуватися клас Admin, ми додали ініціалізацію екземпляра під оголошенням класу.
+
+// Оголошений клас Admin
+// Клас Admin наслідує від класу User
+// Клас Admin містить публічну статичну властивість AccessLevel
+// Клас Admin містить метод constructor з параметром у вигляді об'єкта {email, accessLevel}
+// В класі Admin в конструкторі для властивості email використовується звернення до конструктора батьківського класу
+// Звернення до Admin.AccessLevel.BASIC повертає рядок "basic"
+// Звернення до Admin.AccessLevel.SUPERUSER повертає рядок "superuser"
+
